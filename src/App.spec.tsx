1338b9d19376e8deb45a964aef2ed7dcaafac8ce
Nicolas Moraes de Souza <76184413+NicolasMoraesDeSouza@users.noreply.github.com>
2023-01-13 21:59:13 -0300

    Teste utilizando waitFor

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { getByText, render, waitFor, waitForElementToBeRemoved } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
 
@@ -12,15 +12,32 @@ describe('App', () => {
     });
     it('should be able to render the item', async () => {
         const { getByPlaceholderText } = render( <App /> )
-        const { getByText } = render( <App /> )
+        const { getByText, findByText } = render( <App /> )
 
         const addButton = getByText('Adicionar')
         const addInput = getByPlaceholderText('input')
 
         await userEvent.type(addInput, 'Novo item')
         await userEvent.click(addButton)
+        
+        
+        expect(await findByText('Novo item')).toBeInTheDocument()
 
-        expect(getByText('Novo item')).toBeInTheDocument()
+    })
+    it('should be able to remove the item', async () => {
+        const { getByText, getAllByText, getByPlaceholderText } = render( <App /> )
+        const addButton = getByText('Adicionar')
+        const removeButton = getAllByText('Remover')
 
+
+        userEvent.click(removeButton[0])
+
+        await waitForElementToBeRemoved(() =>{
+            return getByText('Diego')
+        })
+        //ou utilizando a propriedade .not =
+        //await waitFor(() => {
+         //   return expect(getByText('Diego')).not.toBeInTheDocument()
+        //})
     })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ function App() {
   const [NewItem, setNewItem] = useState('')
   const [list, setList] = useState(['Diego', 'Rodz', 'Mayk']) 
   function addToList() {
-    setList(state => [...state, NewItem])
+    setTimeout(() => {
+        setList(state => [...state, NewItem])
+    }, 500)
+  }
+  
+  function removeFromList(item : string) {
+    setTimeout(() => {
+    setList(state =>  state.filter(item => item !== item));
+    }, 500)
   }
  
   return (
@@ -14,7 +22,13 @@ function App() {
      <input placeholder='input' type='text' value={NewItem} onChange={e => setNewItem(e.target.value)}/>
      <button onClick={addToList}>Adicionar</button>
      <ul>
-        {list.map( item => <li key={item}>{item}</li>)}
+        {list.map( item => (
+        <li key={item}>
+          {item}
+          <button onClick={ () => {removeFromList(item)}}>Remover</button>
+        </li>
+        
+        ))}
      </ul>
     </div>
   )
